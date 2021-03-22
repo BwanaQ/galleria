6e76e0d1658be932471a4eaaf8cf16faaf670323
BwanaQ <thunjawax@gmail.com>
2021-03-22 08:51:44 +0300

    add models

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Image(models.Model):
+    image_url = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    timestamp = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
